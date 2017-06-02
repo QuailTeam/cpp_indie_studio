2dccd4439753abe9bb9e4c0a9d3bcf16006687b3
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-06-02 18:59:53 +0200

    ok

--- src/Bomb.cpp
+++ src/Bomb.cpp
@@ -5,7 +5,7 @@
 // Login   <arnaud.alies@epitech.eu>
 // 
 // Started on  Sun May 28 17:29:25 2017 arnaud.alies
-// Last update Fri Jun  2 16:52:30 2017 arnaud.alies
+// Last update Fri Jun  2 18:59:46 2017 arnaud.alies
 //
 
 #include "Bomb.hpp"
@@ -23,6 +23,29 @@ void Bomb::init(Core* core, Map *map, EntityManager* entity_manager)
                    "./res/bomb/Bomb.obj",
                    irr::core::vector3df(320, 320, 320),
                    "./res/bomb/Albedo.png");
+  /*
+  irr::scene::IParticleSystemSceneNode* p = _core->scene->addParticleSystemSceneNode();
+
+  p->setPosition(this->getPos());
+  p->setScale(irr::core::vector3df(4,4,4));
+  p->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+  p->setMaterialFlag(irr::video::EMF_ZWRITE_ENABLE, false);
+  p->setMaterialTexture(0, _core->video->getTexture("./res/fire.bmp"));
+  p->setMaterialType(irr::video::EMT_TRANSPARENT_ADD_COLOR);
+  irr::scene::IParticleEmitter* em = p->createBoxEmitter(irr::core::aabbox3d<irr::f32>(-8,0,-8,8,8,8),
+							 irr::core::vector3df(0.0f,0.02f,0.0f),
+							 80,120,
+							 irr::video::SColor(0,255,255,255),
+							 irr::video::SColor(0,255,255,255), 1100,2000);
+  em->setMinStartSize(irr::core::dimension2df(20.0f, 10.0f));
+  em->setMaxStartSize(irr::core::dimension2df(20.0f, 10.0f));
+  
+  p->setEmitter(em);
+  em->drop();
+  irr::scene::IParticleAffector* paf = p->createFadeOutParticleAffector();
+  p->addAffector(paf);
+  //paf->drop();
+  */
 }
 
 Bomb::~Bomb()
