3856988c5555a631c7801954ba21d3845b496943
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-06-16 11:10:03 +0200

    ok

--- src/Random.cpp
+++ src/Random.cpp
@@ -5,16 +5,26 @@
 // Login   <arnaud.alies@epitech.eu>
 // 
 // Started on  Sun Jun  4 16:03:13 2017 arnaud.alies
-// Last update Fri Jun 16 10:37:02 2017 arnaud.alies
+// Last update Fri Jun 16 10:55:52 2017 arnaud.alies
 //
 
-#include <random>
+#include <cstdlib>
 #include "Random.hpp"
+#include "Core.hpp"
 
 int Random::randint(int min, int max)
 {
+  /*
   std::mt19937 rng;
   rng.seed(std::random_device()());
   std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
   return (dist(rng));
+  */
+  static unsigned int seed = (unsigned int)Core::getTimeMs();
+  int res;
+
+  std::srand(seed);
+  seed += 1;
+  res = (std::rand() % max) + 1;
+  return (res);
 }
