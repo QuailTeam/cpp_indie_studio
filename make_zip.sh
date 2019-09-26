b37e4bd02784334253c066bf5fa165b42dff150a
alies <alies@localhost.localdomain>
2019-09-26 09:18:28 +0200

    conf ignore

--- make_zip.sh
+++ make_zip.sh
@@ -2,7 +2,8 @@
 
 rm -rf release
 mkdir release
-cp -R indie_studio res icon.png libs/* release
+cp -R indie_studio .conf_ignore res icon.png libs/* release
+
 rm indie.zip
 cd release
 zip -r indie.zip *
